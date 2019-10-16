d1c297dc24f89ef9ba6c47506882a33bcba850e4
yumajen <okabe.yuma@jenesty.co.jp>
2019-10-16 09:34:27 +0900

    重複無しの対戦組み合わせ抽選機能を追加

--- src/app/league/league.component.ts
+++ src/app/league/league.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 import { Player } from '../player';
 import { Group } from '../group';
 import { Linkage } from '../linkage';
@@ -15,6 +15,7 @@ export class LeagueComponent implements OnInit {
   players: Player[];
   groups: Group[];
   linkages: Linkage[];
+  matchedInformations: any[] = []; // 各プレイヤーに対しする対戦済みプレイヤー情報を格納する配列
 
   constructor(
     private playersService: PlayersService,
@@ -73,7 +74,7 @@ export class LeagueComponent implements OnInit {
     return eachPlayers;
   }
 
-  getRoundNumber(groupId: number): Number[] {
+  getRoundNumber(groupId: number): number[] {
     // 各グループの人数に応じた回戦数を計算し、回戦数分の空要素を持った配列を返す
     let count = this.linkages.filter((linkage) => {
       return linkage.groupId == groupId;
@@ -82,24 +83,65 @@ export class LeagueComponent implements OnInit {
     return count % 2 == 0 ? Array(count - 1) : Array(count);
   }
 
-  drawLotsCombination(groupId: number): any[] {
+  drawLotsCombination(groupId: number, roundNumber: number): any[] {
     let containedPlayers = [];
     let matches = [];
 
-    // グループ内に含まれるプレイヤー情報をシャッフルして配列に格納する
     containedPlayers = this.getPlayersOfEachGroups(groupId);
-    containedPlayers = this.executeShuffle(containedPlayers);
+    if (roundNumber == 1) {
+      // 各グループの最初のラウンドのみ
+      containedPlayers.forEach((player) => {
+        this.matchedInformations.push(
+          { id: player.id, matched: [] }
+        );
+      });
+    }
+
+    // グループ内に含まれるプレイヤー情報をシャッフルして配列に格納する
+    let shuffledPlayers = this.executeShuffle(containedPlayers);
 
     // 組み合わせの配列に対戦する2人1組の配列をネスティングで格納する
-    while (containedPlayers.length > 1) {
-      matches.push([containedPlayers.shift(), containedPlayers.shift()]);
+    while (shuffledPlayers.length > 1) {
+      // 対戦ペアの1人目を選ぶ
+      let player1 = shuffledPlayers.shift();
+      // player1の対戦済み情報を取得
+      let player1MatchInfo = this.matchedInformations.filter((info) => {
+        return info.id == player1.id;
+      });
+      // player1と既に対戦済みのプレイヤー情報を取得
+      let alreadyMatched = player1MatchInfo[0].matched;
+
+      let player2: Player;
+      for (let player of shuffledPlayers) {
+        if (alreadyMatched.indexOf(player) == -1) {
+          player2 = shuffledPlayers.splice(shuffledPlayers.indexOf(player), 1)[0];
+          break;
+        }
+      }
+      matches.push([player1, player2]);
+
+      // player1, player2を互いのmatchedに格納する
+      this.matchedInformations.forEach((info) => {
+        if (info.id == player1.id) {
+          info.matched.push(player2);
+        }
+        if (info.id == player2.id) {
+          info.matched.push(player1);
+        }
+      });
+    }
+
+    if ((containedPlayers.length % 2 == 0 && roundNumber == containedPlayers.length - 1) ||
+      (containedPlayers.length % 2 != 0 && roundNumber == containedPlayers.length)) {
+      // 各グループの最後のラウンドのみ
+      this.matchedInformations = [];
     }
 
     return matches;
   }
 
   executeShuffle(containedPlayers: Player[]): Player[] {
-    let array = containedPlayers;
+    let array = Array.from(containedPlayers);
     for (let i = array.length - 1; i >= 0; i--) {
       let rand = Math.floor(Math.random() * (i + 1));
       [array[i], array[rand]] = [array[rand], array[i]];
